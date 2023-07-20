c961f186f0955b951e067d605e1d5f5bbcb6fcd1
Ismael Ait El Kamel <ismael.aitelkamel17@gmail.com>
2023-07-20 19:05:40 +0100

    Added start and turn off sounds and a condition to let the car move only when the engine is started.

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ let car = {
     x: 210,
     y: 200,
     width: 100,
-    height: 80
+    height: 80,
+    starting : false
 };
 let ornageTreeImage = new Image();
 ornageTreeImage.src= "assets/img/Picture3.png"; 
@@ -43,27 +44,48 @@ let drawTrees = () => {
     game.drawImage(bigTreeImage, 150, 200, 80, 70);
 }
 
+let carEngingStartSound = new Audio();
+carEngingStartSound.src = "assets/sounds/car-engine-starting.mp3";
+
+let carTurnOffSound = new Audio();
+carTurnOffSound.src = "assets/sounds/car-turn-off.mp3";
+
 let steps = 5;
 let Movement = (event) => {
-switch (event.keyCode) {
-    // up btn
-    case 38: 
-        car.y -= steps;
-        break;
-    // down btn
-    case 40: 
-        car.y += steps;
-        break;
-    // left btn
-    case 37:
-        car.x -= steps;
-        break;
-    // right btn
-    case 39: 
-        car.x += steps;
-        break;
+    if (car.starting === true){
+        switch (event.keyCode) {
+            // up btn
+            case 38: 
+                car.y -= steps;
+                break;
+            // down btn
+            case 40: 
+                car.y += steps;
+                break;
+            // left btn
+            case 37:
+                car.x -= steps;
+                break;
+            // right btn
+            case 39: 
+                car.x += steps;
+                break;
+        }
     }
 }
+
+let carEngine = () => {
+    if (car.starting === false) {
+        carEngingStartSound.play();
+        car.starting = true;
+    } else {
+        carTurnOffSound.play()
+        car.starting = false;
+    }
+};
+
+canvas.addEventListener("dblclick", carEngine);
+
 carImage.onload = function() {
     document.addEventListener("keydown", Movement);
     setInterval(draw, 20);
