4c7edd469530eba9c608ae5d00e8ddad098dfe81
Ismael Ait El Kamel <ismael.aitelkamel17@gmail.com>
2023-07-21 18:26:38 +0100

    Imlemented a 'you won' message to display when the car successfully reaches the finish line. the gme now automatically restart after 3 seconds to play again after winning. Additionaly, added collision detection to prevent car from moving over trees.

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ let canvas = document.getElementById("canvas");
 let game = canvas.getContext("2d");
 
 let carImage = new Image();
-carImage.src = "assets/img/maxresdefault .png"; 
+carImage.src = "assets/img/maxresdefault.png"; 
 let car = {
-    x: 210,
-    y: 200,
-    width: 100,
-    height: 80,
+    x: 240,
+    y: 320,
+    width: 50,
+    height: 60,
     starting : false
 };
 let ornageTreeImage = new Image();
@@ -22,26 +22,47 @@ bigTreeImage.src = "assets/img/Picture1.png";
 let finishLineImage = new Image();
 finishLineImage.src= "assets/img/Picture5.png";
 
-
+let winner = false;
 let draw = () => {
     game.clearRect(0, 0, canvas.width, canvas.height);
-    game.drawImage(finishLineImage, 0, 35, 80, 100);
+    game.drawImage(finishLineImage, 0, 70, 50, 100);
     drawTrees();
     game.drawImage(carImage, car.x, car.y, car.width, car.height);
 
+    // win message
+    if (!winner && car.x === 0 && car.y >= 70 && car.y <= 100) {
+        // Display "You won" message
+        game.font = "30px times";
+        game.fillStyle = "black";
+        game.fillText("You won!", canvas.width / 2 - 60, canvas.height / 2);
+        winner = true;
+        car.starting = false; // to stop the car movement
+
+        // restart after 5 seconds
+        setTimeout(restartGame, 3000); 
+    }
 }
 
+let restartGame = () => {
+    car.x = 240;
+    car.y = 320;
+    car.starting = false;
+    winner = false;
+
+    setInterval(draw, 20);
+};
+
 let drawTrees = () => {
-    game.drawImage(ornageTreeImage, 40, 100, 60, 60);
-    game.drawImage(ornageTreeImage, 300, 100, 60, 60);
+    game.drawImage(ornageTreeImage, 40, 160, 60, 60);
+    game.drawImage(ornageTreeImage, 300, 160, 60, 60);
 
-    game.drawImage(greenTreeImage, 35, -10, 80, 70);
-    game.drawImage(greenTreeImage, 300, 0, 80, 70);
-    game.drawImage(greenTreeImage, 150, 100, 80, 70);
+    game.drawImage(greenTreeImage, 35, 0, 80, 70);
+    game.drawImage(greenTreeImage, 270, 10, 80, 70);
+    game.drawImage(greenTreeImage, 150, 150, 80, 70);
 
-    game.drawImage(bigTreeImage, 150, -10, 80, 70);
-    game.drawImage(bigTreeImage, 300, 200, 80, 70);
-    game.drawImage(bigTreeImage, 150, 200, 80, 70);
+    game.drawImage(bigTreeImage, 150, 0, 80, 70);
+    game.drawImage(bigTreeImage, 300, 260, 80, 70);
+    game.drawImage(bigTreeImage, 150, 260, 80, 70);
 }
 
 let carEngingStartSound = new Audio();
@@ -50,29 +71,67 @@ carEngingStartSound.src = "assets/sounds/car-engine-starting.mp3";
 let carTurnOffSound = new Audio();
 carTurnOffSound.src = "assets/sounds/car-turn-off.mp3";
 
+let checkCollision = (x, y) => {
+    // 
+    let trees = [
+        // orange trees 
+        { x: 40, y: 160, width: 60, height: 60 },
+        { x: 300, y: 160, width: 60, height: 60 },
+        // green trees
+        { x: 35, y: 0, width: 80, height: 70 },
+        { x: 270, y: 10, width: 80, height: 70 },
+        { x: 150, y: 150, width: 80, height: 70 },
+        // big trees
+        {x: 150, y: 0, width: 80, height: 70 },
+        {x: 300, y: 260, width: 80, height: 70 },
+        {x: 150, y: 260, width: 80, height: 70 },
+    ];
+
+    for (let tree of trees) {
+        if (
+            x < tree.x + tree.width &&
+            x + car.width > tree.x &&
+            y < tree.y + tree.height &&
+            y + car.height > tree.y
+        ) {
+            return true; 
+        }
+    }
+    return false; 
+};
+
 let steps = 5;
 let Movement = (event) => {
-    if (car.starting === true){
+    if (car.starting === true) {
+        let carDx = car.x;
+        let carDy = car.y;
+
         switch (event.keyCode) {
             // up btn
-            case 38: 
-                car.y -= steps;
+            case 38:
+                carDy -= steps;
                 break;
             // down btn
-            case 40: 
-                car.y += steps;
+            case 40:
+                carDy += steps;
                 break;
             // left btn
             case 37:
-                car.x -= steps;
+                carDx -= steps;
                 break;
             // right btn
-            case 39: 
-                car.x += steps;
+            case 39:
+                carDx += steps;
                 break;
         }
+        if (carDx >= 0 && carDx + car.width <= canvas.width && carDy >= 0 && carDy + car.height <= canvas.height) {
+            if (!checkCollision(carDx, carDy)) {
+                car.x = carDx;
+                car.y = carDy;
+            }
+        }
     }
-}
+};
 
 let carEngine = () => {
     if (car.starting === false) {
